3a6e9fc5c3d24b9fdf24a3b276c6e7b672fa30eb
Pablo Soares <pablo@mw8solucoes.com.br>
2017-07-05 13:25:26 -0300

    Add main Java File

---
+++ android/src/main/java/br/com/universomw8/rnmediawesome/RNMediawesomePackage.java
@@ -0,0 +1,33 @@
+package br.com.universomw8.rnmediawesome;
+
+import java.util.*;
+
+import android.app.Activity;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+public class RNMediawesomePackage implements ReactPackage {
+  private Activity mCurrentActivity;
+
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    List<NativeModule> modules = new ArrayList<>();
+
+    modules.add(new RNMediawesome(reactContext));
+    return modules;
+  }
+
+  @Override
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Arrays.<ViewManager>asList();
+  }
+}
--- rnmediawesome.js
+++ rnmediawesome.js
@@ -7,21 +7,25 @@ var RNMediawesome = require('react-native').NativeModules.RNMediawesome;
 module.exports = {
   createPlaylist: function(filePaths, promise) {
     return RNMediawesome.createPlaylist;
-  }
+  },
 
   getPlaylist: function(id, promise) {
     return RNMediawesome.getPlaylist;
-  }
+  },
+
+  destroyPlaylist: function(id, promise) {
+    return RNMediawesome.destroyPlaylist;
+  },
 
   getAllPlaylists: function(promise) {
     return RNMediawesome.getAllPlaylists;
-  }
+  },
 
-  startPlaylists: function(id, promise) {
+  startPlaylist: function(id, promise) {
     return RNMediawesome.startPlaylists;
-  }
+  },
 
   stopPlaylist: function(id, promise) {
     return RNMediawesome.stopPlaylist;
-  }
-}
+  },
+};
